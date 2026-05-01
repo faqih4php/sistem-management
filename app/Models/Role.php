19399f6ba7d39d448ae53aef4ddbc569201abec9
faqih4php <faqihaverroes@gmail.com>
2026-05-01 21:12:17 +0700

    feat: initialize core database schema and Eloquent models for users, roles, projects, and tasks

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    protected $guarded = ['id'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
